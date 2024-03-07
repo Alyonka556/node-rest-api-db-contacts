53bafd3ef3c39cef4f6520a26de4576d286de205
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2024-03-07 22:57:50 +0100

    hw-node-rest-api-db-contacts

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, setUpdateSetting } from "./hooks.js";
+import { emailRegexp } from "../constants/regexp.js";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+      minlength: 3,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", setUpdateSetting);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const User = model("user", userSchema);
+
+export default User;
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,21 +1,36 @@
 import Contact from "../models/Contact.js";
 
-export const listContacts = () => Contact.find();
+export const listContacts = () => Contact.find({}, "-createdAt, updatedAt");
 
-export const getContactById = async (contactId) => {
+export const getContactsByFilter = (filter, query = {}) =>
+  Contact.find(filter, "-createdAt, updatedAt", query);
+
+export const getContactsCountByFilter = (filter) =>
+  Contact.countDocuments(filter);
+export const getContactById = (contactId) => {
   return Contact.findOne({ _id: contactId });
 };
 
-export const removeContact = async (contactId) =>
+export const getContactByFilter = (filter) => {
+  return Contact.findOne(filter);
+};
+
+export const removeContact = (contactId) =>
   Contact.findByIdAndDelete(contactId);
 
-export const addContact = async (data) => Contact.create(data);
+export const removeContactByFilter = (filter) =>
+  Contact.findOneAndDelete(filter);
 
-export const updateContactById = async (contactId, data) =>
+export const addContact = (data) => Contact.create(data);
+
+export const updateContactById = (contactId, data) =>
   Contact.findByIdAndUpdate(contactId, data, {
     new: true,
     runValidators: true,
   });
+
+export const updateContactByFilter = (filter, data) =>
+  Contact.findOneAndUpdate(filter, data);
 
 export const getUpdateStatusContact = async (contactId, data) =>
   Contact.findByIdAndUpdate(contactId, data, {
